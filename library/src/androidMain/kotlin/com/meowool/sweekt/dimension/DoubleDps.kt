3ebadd91bbb69d15c4df2e3ae7654aec90b686f3
RinOrz <rinorz666@gmail.com>
2021-09-25 16:35:45 +0800

    Add `dp` conversion of primitive number types

---
+++ library/src/androidMain/kotlin/com/meowool/sweekt/dimension/DoubleDps.kt
@@ -0,0 +1,76 @@
+package com.meowool.sweekt.dimension
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+/**
+ * Converts this density-independent pixels to the int value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ * @author 凛 (https://github.com/RinOrz)
+ */
+val Double.dpInt: Int get() = (this * Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * Converts this density-independent pixels to the int value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Double.dpInt(context: Context): Int = (this * context.resources.displayMetrics.density).roundToInt()
+
+/**
+ * Converts this density-independent pixels to the long value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Double.dpLong: Long get() = (this * Resources.getSystem().displayMetrics.density).roundToLong()
+
+/**
+ * Converts this density-independent pixels to the long value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Double.dpLong(context: Context): Long = (this * context.resources.displayMetrics.density).roundToLong()
+
+/**
+ * Converts this density-independent pixels to the float value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Double.dpFloat: Float get() = (this * Resources.getSystem().displayMetrics.density).toFloat()
+
+/**
+ * Converts this density-independent pixels to the float value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Double.dpFloat(context: Context): Float = (this * context.resources.displayMetrics.density).toFloat()
+
+/**
+ * Converts this density-independent pixels to the double value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Double.dpDouble: Double get() = this * Resources.getSystem().displayMetrics.density
+
+/**
+ * Converts this density-independent pixels to the double value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Double.dpDouble(context: Context): Double = this * context.resources.displayMetrics.density
---
+++ library/src/androidMain/kotlin/com/meowool/sweekt/dimension/LongDps.kt
@@ -0,0 +1,76 @@
+package com.meowool.sweekt.dimension
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+/**
+ * Converts this density-independent pixels to the int value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ * @author 凛 (https://github.com/RinOrz)
+ */
+val Long.dpInt: Int get() = (this * Resources.getSystem().displayMetrics.density).roundToInt()
+
+/**
+ * Converts this density-independent pixels to the int value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Long.dpInt(context: Context): Int = (this * context.resources.displayMetrics.density).roundToInt()
+
+/**
+ * Converts this density-independent pixels to the long value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Long.dpLong: Long get() = (this * Resources.getSystem().displayMetrics.density).roundToLong()
+
+/**
+ * Converts this density-independent pixels to the long value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Long.dpLong(context: Context): Long = (this * context.resources.displayMetrics.density).roundToLong()
+
+/**
+ * Converts this density-independent pixels to the float value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Long.dpFloat: Float get() = this * Resources.getSystem().displayMetrics.density
+
+/**
+ * Converts this density-independent pixels to the float value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Long.dpFloat(context: Context): Float = this * context.resources.displayMetrics.density
+
+/**
+ * Converts this density-independent pixels to the double value stands device pixels.
+ *
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+val Long.dpDouble: Double get() = (this * Resources.getSystem().displayMetrics.density).toDouble()
+
+/**
+ * Converts this density-independent pixels to the double value stands device pixels by given [context].
+ *
+ * @see Context.getResources
+ * @see Resources.getDisplayMetrics
+ * @see DisplayMetrics.density
+ */
+fun Long.dpDouble(context: Context): Double = (this * context.resources.displayMetrics.density).toDouble()
