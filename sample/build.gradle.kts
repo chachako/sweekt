532d0e2614c2ac1b66fceb5789ddbe618f38c54b
RinOrz <rinorz666@gmail.com>
2021-09-26 13:17:17 +0800

    Update gradle script

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,10 +1,27 @@
+/*
+ * Copyright (c) 2021. The Meowool Organization Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+
+ * In addition, if you modified the project, you must include the Meowool
+ * organization URL in your code file: https://github.com/meowool
+ *
+ * 如果您修改了此项目，则必须确保源文件中包含 Meowool 组织 URL: https://github.com/meowool
+ */
 plugins {
   id("com.android.application")
   kotlin("android")
 }
-
-group = "com.meowool.toolkit"
-version = "1.0-SNAPSHOT"
 
 dependencies {
   implementation(project(":library"))
