dd57fa50dc058c4005dbb356cb1fca02278cef0a
RinOrz <rinorz666@gmail.com>
2021-11-23 21:16:46 +0800

    Resolve https://github.com/RinOrz/sweekt/issues/7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,9 +20,6 @@ buildscript {
     // Check for updates every build
     resolutionStrategy {
       force("com.android.tools.build:gradle:4.2.1")
-      eachDependency {
-        if (requested.group == "org.jetbrains.kotlin") useVersion("1.5.31")
-      }
       cacheChangingModulesFor(0, TimeUnit.SECONDS)
     }
   }
@@ -44,18 +41,10 @@ gradleToolkit {
     dokka(DokkaFormat.Html) {
       outputDirectory.set(rootDir.resolve("docs/apis"))
     }
-    configurations.all {
-      // Check for updates every build
-      resolutionStrategy {
-        eachDependency {
-          if (requested.group == "org.jetbrains.kotlin") useVersion("1.5.31")
-        }
-      }
-    }
   }
   publications {
     data {
-      val baseVersion = "0.1.0-1.5.31"
+      val baseVersion = "0.1.0"
       version = "$baseVersion-LOCAL"
       // Used to publish non-local versions of artifacts in CI environment
       versionInCI = "$baseVersion-SNAPSHOT"
